0f57731f547567b39819dec5993c6e03b0ae6aca
mesbahi-01 <mesbahi.ed@outlook.com>
2022-09-22 01:11:49 +0100

    fix errors related to  fetching data

--- app/src/App.js
+++ app/src/App.js
@@ -2,36 +2,85 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import Chart from './components/Chart';
 import Loading from './components/Loading';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Button from 'react-bootstrap/Button';
 import Forcast from './components/Forcast';
 import Error from './components/Error';
+import { ButtonGroup } from '@mui/material';
+import Button from '@mui/material/Button';
+import { format } from "date-fns";
 
 const App = () => {
 
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-
   const [measurements, setMeasurements] = useState({});
-  const [field, setField] = useState("field1");
-
+  const [field, setField] = useState("field3");
+  const [weather_data, setWeatherData] = useState({})
+  const custom_button = {
+    width: '90vw',
+    fontSize: '1rem',
+    fontWeight: 'meduim',
+    fontFamily: 'Lucida Sans',
+    margin: '0px',
+    border: '0px solid blue',
+  }
+  // a function to retrieve data
   const fetchMeasurements = async () => {
-    setIsLoading(true);
-    const response = await fetch("/measurements")
+    const response = await fetch("api/measurements")
     const data = await response.json()
     setMeasurements(data)
-    setIsLoading(false)
-  };
+  }
+  // to retrive data 
+  const fetchWeather = () => {
+    // Checking if geolocation is available
+    if ("geolocation" in navigator) {
+      // get coordinates
+      navigator.geolocation.getCurrentPosition(function (position) {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        localStorage.setItem("summerx_lat", lat);
+        localStorage.setItem("summerx_long", long);
+        localStorage.setItem("summerx_locationPermission", true);
+        const weather_api_url = `api/weather/${lat},${long}`
+        fetch(weather_api_url)
+          .then((res) => res.json())
+          .then((res) => {
+            const feels_like = parseInt(res.main.feels_like);
+            const humidity = parseInt(res.main.humidity);
+            const sunRise_ = format(new Date(res.sys.sunrise), 'p');
+            const sunSet_ = format(new Date(res.sys.sunset + 47220000), 'p');
+            setWeatherData({
+              feels_like: feels_like,
+              humidity: humidity,
+              sunRise_: sunRise_,
+              sunSet_: sunSet_
+            })
 
+          }).catch((error) => console.log(error))
+      })
+    } else {
+      alert("Unable to get Location permission");
+    }
+  }
+  // a functoin to retriev all the data
+  const fetchAllData = () => {
+    fetchMeasurements();
+    fetchWeather();
+  }
   // retriave the data after we render the view
   useEffect(() => {
-    fetchMeasurements();
+    fetchAllData();         // update Measurments each 10 minutes
+    setTimeout(() => setIsLoading(false), 2000)
+    const timerID_1 = setInterval(fetchMeasurements, 60000);
+    const timerID_2 = setInterval(fetchWeather, 600000); // update the weather every 10 minutes
+    // clear intervals after unmounting
+    return () => {
+      clearInterval(timerID_1);
+      clearInterval(timerID_2);
+    }
+
   }, []);
 
-  // call the function "fetchMeasurments" every 15 minutes
-  setInterval(fetchMeasurements, 900000);
-
-  // if we are fetching the data from Thinkspeak display the LOADING view
+  //  display the LOADING view in case of fetching the data 
   if (isLoading) {
     return (
       <main className='loading'>
@@ -52,12 +101,20 @@ const App = () => {
   // display the FORCAST bar + the BUTTONS to change the graphe + the CHART 
   return <>
     <main className="layout" >
-      <Forcast></Forcast>
-      <ButtonGroup vertical className='btns'>
-        <Button onClick={() => setField("field1")}>temperature </Button>
-        <Button onClick={() => setField("field2")} >humidity</Button>
-        <Button onClick={() => setField("field3")}>soil moisture</Button>
-        <Button onClick={() => setField("field4")}>light intensity</Button>
+      <Forcast weather_data={weather_data}></Forcast>
+      <ButtonGroup disableElevation orientation="vertical"
+        aria-label="vertical contained button group"
+        variant="contained"
+        sx={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+        }}
+      >
+        <Button sx={custom_button} onClick={() => setField("field3")}>soil moisture</Button>
+        <Button sx={custom_button} onClick={() => setField("field2")}>humidity</Button>
+        <Button sx={custom_button} onClick={() => setField("field4")}>light intensity</Button>
+        <Button sx={custom_button} onClick={() => setField("field1")}>temperature</Button>
       </ButtonGroup>
       <Chart measurements={measurements} field={field} />
     </main>
